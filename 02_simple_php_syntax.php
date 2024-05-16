3f6f2f188397c7add70330e7719fa2f7b3d5bb5f
Elloisa Degula <97028224+EMD-001@users.noreply.github.com>
2024-05-16 22:07:19 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php
@@ -0,0 +1,35 @@
+<?php 
+
+$name = "Elloisa Degula";
+$age = 21;
+$address = "F.Simeon, Ragay, Camarines Sur";
+// $hobbies = "Dancing, Volleyball";
+// $pet_peeve = "Attention seeker and Gold diggger";
+
+echo $name . "</br>";
+echo $age . "</br>";
+echo $address . "</br>";
+echo $hobbies . "</br>";
+echo $pet_peeve . "</br>";
+
+define("Name", "Elloisa Degula");
+define("Age", 21);
+define("Address", "F.Simeon, Ragay, Camarines Sur");
+define("Hobbies", "Dancing, Volleyball");
+define("Pet_Peeve", "Attention seeker and Gold diggger");
+
+print Name . "</br>";
+print Age . "</br>";
+print Address . "</br>";
+print Hobbies . "</br>";
+print Pet_Peeve . "</br>";
+
+var_dump(Name);
+var_dump(Age);
+var_dump(Address);
+var_dump(Hobbies);
+var_dump(Pet_Peeve);
+
+/*
+They're taking advantage of another person's status, and they seek attention to satisfy themselves.
+*/
